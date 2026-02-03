47ff119439f82cdab42f29e3a3c526404b3d1f83
Sanjeev Midda <s.midda1@gmail.com>
2026-02-03 22:08:32 +0000

    Adding array of words to use, creating state to hold our status, and adding conditional styling to our App component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
 import { useState } from "react";
 import "./index.css";
+import Words from "./components/Words";
 
 function App() {
   const [status, setStatus] = useState(false);
 
+  const word = [
+    "ON",
+    "OFF",
+    "ONE",
+    "TWO",
+    "THREE",
+    "FOUR",
+    "FIVE",
+    "SIX",
+    "SEVEN",
+    "EIGHT",
+    "NINE",
+    "TEN",
+  ];
+
   return (
-    <div className="App">
-      <h1>ON</h1>
+    <div
+      className="App"
+      style={{ backgroundColor: status ? "black" : "white" }}
+    >
+      <Words
+        status={status}
+        setStatus={setStatus}
+        word={word[Math.floor(Math.random() * word.length)]}
+      />
+      <Words
+        status={status}
+        setStatus={setStatus}
+        word={word[Math.floor(Math.random() * word.length)]}
+      />
+      <Words
+        status={status}
+        setStatus={setStatus}
+        word={word[Math.floor(Math.random() * word.length)]}
+      />
+      {/* <h1
+        style={{ color: status ? "white" : "blue" }}
+        onClick={() => setStatus(!status)}
+      >
+        {status ? "ON" : "OFF"}
+      </h1> */}
     </div>
   );
 }
